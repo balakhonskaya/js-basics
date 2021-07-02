6d8b0ad8e8eecf0a17173dcda6c69db971b2cca6
balakhonkaya <anthemion@yandex.ru>
2021-07-02 16:27:26 +0200

    add task 1

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 //JS Digits
 // Task 1 
 
+function calculateSquare(x1, y1, x2, y2) {
+    let sideX = Math.abs(x2 - x1);
+    let sideY = Math.abs(y2 - y1);
+    let square = sideX*sideY;
+    return square;
+}
