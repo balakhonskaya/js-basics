d45d4c64e09c78e94838b0d1557aa45f53296eaa
balakhonkaya <anthemion@yandex.ru>
2021-07-03 10:37:01 +0200

    correct the second task

--- main.js
+++ main.js
@@ -8,15 +8,15 @@ function calculateSquare(x1, y1, x2, y2) {
     return square;
 }
 
-//Task 2 Compare a and b with precision n
+//Task 2 Compare fractional part of a numberы a and b with precision n
 
 function compareDigits(a,b,n) {
-    let aNormalized = Math.round(a * Math.pow(10, n));
-    let bNormalized = Math.round(b * Math.pow(10, n));
-    console.log('Число a', aNormalized);
-    console.log('Число b', bNormalized);
-    console.log('Исходные числа равны', a===b);
-    console.log('Числа равны с точностью n', aNormalized === bNormalized);
+    let aNormalized = (a%1).toFixed(n);
+    let bNormalized = (b%1).toFixed(n);
+    console.log('Дробная часть a', aNormalized);
+    console.log('Дробная часть b', bNormalized);
+    console.log('Дробные части равны', a===b);
+    console.log('Дробные части равны с точностью n', aNormalized === bNormalized);
     console.log('Первое число больше второго', aNormalized > bNormalized);
     console.log('Второе число больше первого',aNormalized < bNormalized);
 }
